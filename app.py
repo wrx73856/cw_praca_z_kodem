8d2a439164c7f214eefcbee1a28a3eba6cc4236d
wrx73856 <wrx73856@student.wsb.wroclaw.pl>
2022-04-12 17:29:15 +0200

    add Makefile, change name

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='Welcome', username='WSB')
+    return render_template('index.html', title='Welcome', username='Mikolaj')
 
 
 @app.route('/index/<name>')
